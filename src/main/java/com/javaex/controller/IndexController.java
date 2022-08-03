3a0b22d69783d65bd01bc9bb31b484a6138e482f
subintoto <subintoto@naver.com>
2022-08-03 12:45:17 +0900

    2022-08-03

--- src/main/java/com/javaex/controller/IndexController.java
+++
@@ -1,8 +0,0 @@
-package com.javaex.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class IndexController {
-
-}
---
+++ src/main/java/com/javaex/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.javaex.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+	
+/* @Autowired 잊지 말자... */
+//필드
+//생성자
+//메소드
+//메소드-일반
+}
