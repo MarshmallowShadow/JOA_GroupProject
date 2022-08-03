1f0ef9a06c132718a2a94b309e94a1ddd7fa4ebd
GyeongAe Kim <kgae128@gmail.com>
2022-08-03 16:52:42 +0900

    mmm

--- webapp/assets/js/my-page/bookmark.js
+++ webapp/assets/js/my-page/bookmark.js
@@ -1,19 +1,19 @@
 /*준비가 끝나면*/
 $(document).ready(function(){
-    console.log('즐겨찾기');
-    /*$(".dislike-modal").hide();
-    $(".bye-bookmark-modal").hide();
+	console.log('즐겨찾기');
+	$(".course-like-cancel").hide();
+	$(".course-bookmark-cancel").hide();
     
 	
 	$(".like-btn").click(function(){
-      console.log("좋아요해제");
-      $(".dislike-modal").modal("show");
+		console.log("좋아요해제");
+		$(".course-like-cancel").modal("show");
    	});
    
-   	$(".bookmark-btn").click(function(){
-      console.log("즐겨찾기해제");
-      $(".bye-bookmark-modal").modal("show");
-   	});
-   	*/
+	$(".bookmark-btn").click(function(){
+		console.log("즐겨찾기해제");
+		$(".course-bookmark-cancel").modal("show");
+	});
+   	
    	
 });
