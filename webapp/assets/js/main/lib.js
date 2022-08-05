59bbcbe859d539be6f9d18fa6f2f61b8e37050eb
subintoto <subintoto@naver.com>
2022-08-06 01:27:19 +0900

    날씨날씨날씨날씨날씨

---
+++ webapp/assets/js/main/lib.js
@@ -0,0 +1,25 @@
+/**
+ * API 연결
+ */
+ 
+ $api_key = "ddeb92652c34f9e77a6961c434afa555";
+ $search_city = urlencode($_POST["search_city"]);	//사용자가 요청한 도시 정보, urlencode UTF-8언어를 변경
+ /*$lat = $_POST["let"];
+ $lon = $_POST["lng"];*/
+ 
+ /*api 접속하는 주소*/
+ $url = "api.openweathermap.org/data/2.5/weather?q=" + $search_city + "&appid=" + $api_key + "&units=metric";
+ 
+ $is_post = false;
+ $ch = url_init();
+ curl_setopt($ch,CURLOPT_URL, $url);
+ curl_setopt($ch,CURLOPT_POST, $is_post);
+ curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+ curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, 0);  
+ $response =  curl_exec($ch);
+ $state_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+ curl_close($ch);
+ if($state_code == 200)
+ {
+	document.write ($response);
+ }
--- webapp/assets/js/main/weather.js
+++ webapp/assets/js/main/weather.js
@@ -1,43 +1,26 @@
 /*
  * 날씨 api 
  */
-
-function showLocation(event) {
-  var latitude = event.coords.latitude 
-  var longitude = event.coords.longitude
-  document.querySelector("#latitude").textContent = latitude
-  document.querySelector("#longitude").textContent = longitude 
-
-  
-  let apiKey = "ddeb92652c34f9e77a6961c434afa555"
-  let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude 
-                + "&lon=" + longitude 
-                + "&appid=" + apiKey; 
-
-	let options = { method: 'GET' }
-	
-  ajax(weatherUrl, options).then((response) => {
-		
-      console.log(response)
-      
-      let icon = response.weather[0].icon
-      let iconUrl = "http://openweathermap.org/img/wn/01d@2x.png"	/* 이미지 임시 */ 
-      let img = document.querySelector("#wicon")
-      img.src = iconUrl 
-      
-      document.querySelector("#tempr").textContent = (response.main.temp) + "(" + (response.main.temp - 273) + ")" // 현재 온도
-    }).catch((error) => {
-      console.log(error)
-    })
+function goSearch () 	//현재 위치로 검색
+{
+	var SERCH_CITY = $("city").val();
+	if(!SERCH_CITY){
+		alert("먼저 도시를 입력하세요");		//SERCH_CITY값 입력하지 않을 경우 경고장
+		return false;					//그럴 경우 더이상 진행 불가
+	}
+	else {								//API 엔진과 소통
+		$ajax({
+			type: "POST",
+			dataType: "JSON",
+			url: "lib.js",
+			data: {"search_ciy" : SERCH_CITY},	//검색하고자 하는 도시 정보
+			cache: false,
+			async: false
+		})
+		.done(function(result) {
+			//ajax 통신이 성공한 경우
+			console.log(result);
+			alert("현재온도" + result.main.temp);
+		});
+	}
 }
-
-function showError(event) {
-  alert("위치 정보를 얻을 수 없습니다.")
-}
-
-window.addEventListener('load', () => { 
-  if(window.navigator.geolocation) {
-     window.navigator.geolocation.getCurrentPosition(showLocation,showError)
-  }
-
-})
