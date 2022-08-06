02d3aaa1226d25a46f1b76b007af3449d7eedd60
subintoto <subintoto@naver.com>
2022-08-06 16:07:17 +0900

    2022-08-06

--- src/main/java/com/javaex/vo/BestVo.java
+++
@@ -1,7 +0,0 @@
-package com.javaex.vo;
-
-public class BestVo {
-	
-
-
-}
--- webapp/assets/js/main/weather.js
+++ webapp/assets/js/main/weather.js
@@ -3,17 +3,17 @@
  */
 function goSearch () 	//현재 위치로 검색
 {
-	var SERCH_CITY = $("city").val();
-	if(!SERCH_CITY){
+	var SEARCH_CITY = $("#city").val();
+	if(!SEARCH_CITY){
 		alert("먼저 도시를 입력하세요");		//SERCH_CITY값 입력하지 않을 경우 경고장
 		return false;					//그럴 경우 더이상 진행 불가
 	}
 	else {								//API 엔진과 소통
-		$ajax({
+		$.ajax({
 			type: "POST",
 			dataType: "JSON",
 			url: "lib.js",
-			data: {"search_ciy" : SERCH_CITY},	//검색하고자 하는 도시 정보
+			data: {"search_city" : SEARCH_CITY},	//검색하고자 하는 도시 정보
 			cache: false,
 			async: false
 		})
