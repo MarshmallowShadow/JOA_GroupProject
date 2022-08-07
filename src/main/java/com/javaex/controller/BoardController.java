d7d2638026641dff6d0f2c9d234b9ba69c937c11
가은 <rkdms8228@naver.com>
2022-08-07 23:35:15 +0900

    페이징

--- src/main/java/com/javaex/controller/BoardController.java
+++ src/main/java/com/javaex/controller/BoardController.java
@@ -1,17 +1,37 @@
 package com.javaex.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.javaex.service.BoardService;
 
 @Controller
 public class BoardController {
 	
+	//필드
+	@Autowired
+	BoardService boardService;
+	//생성자
+	
+	
+	//메소드-gs
+	
+	
+	//메소드-일반
 	//게시판 리스트
 	@RequestMapping(value="/board", method = {RequestMethod.GET, RequestMethod.POST})
-	public String board() {
+	public String board(Model model, @RequestParam(value="crtPage", required = false, defaultValue = "1")int crtPage) {
 		
-		 System.out.println("BoardController > board");
+		System.out.println("BoardController > board");
+		 
+		Map<String, Object> pMap = boardService.board(crtPage);
+		model.addAttribute("pMap", pMap);
 		
 		return "board/board";
 	}
