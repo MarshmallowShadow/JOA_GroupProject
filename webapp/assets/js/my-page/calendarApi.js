a8314e753b128c74e36401778a78afafb070b7fb
MarshmallowShadow <choiboseung@naver.com>
2022-08-08 15:00:15 +0900

    누나 알아서 해

--- webapp/assets/js/my-page/calendarApi.js
+++ webapp/assets/js/my-page/calendarApi.js
@@ -1,22 +1,11 @@
 /*달력 API*/
+var recordList;
+
 $(document).ready(function(){
    	
    	var userNo = '${authUser.userNo}';
    	
-   	$.ajax({
-		url : "${pageContext.request.contextPath}/api/my-page/get-record-list", //컨트롤러 RequestMapping url 작성하기
-		type : "post",
-		contentType : "application/json", //@RequestBody로 파라미터 가져오기 위해 필수 (정보 보낼거 없으면 필요없음)
-		data : JSON.stringify(userNo), //@RequestBody로 데이터 보낼때 필수 (정보 보낼거 없으면 필요없음)
-			//data: Vo //@ModelAttribute나 @RequestParam으로 데이터 보낼때 이용 (정보 보낼거 없으면 필요없음)
-		dataType : "json",
-		success : function(result){
-			//컨트롤러 함수 실행 후 코드
-		},
-		error : function(XHR, status, error) {
-			console.error(status + " : " + error);
-		}
-	});
+   	
 });
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -31,7 +20,9 @@ document.addEventListener('DOMContentLoaded', function() {
 		businessHours: true,	//주말 구분;
 		dayMaxEvents: false, // allow "more" link when too many events
 		
-		events: [	//나의 기록 여기다가 추가해야할 듯?
+		events: recordList
+		
+		/*[	//나의 기록 여기다가 추가해야할 듯?
 		//ajax 데이터 불러올 부분///////////////////////////////
 			{
 	          title: '테스트 코스',
@@ -55,6 +46,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	          start: '2022-08-18'
 	        }
 		]
+		*/
 	});
 	calendar.render();
 	
