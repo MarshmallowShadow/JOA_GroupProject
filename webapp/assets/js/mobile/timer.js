952b880341673fc1273b50d91abf87c11e944266
MarshmallowShadow <choiboseung@naver.com>
2022-08-16 17:47:55 +0900

    타이머 완료

--- webapp/assets/js/mobile/timer.js
+++ webapp/assets/js/mobile/timer.js
@@ -2,11 +2,63 @@
  * 
  */
 
-$(document).ready(function(){
-	
-});
+var deciseconds = 0;
+var seconds = 0;
+var minutes = 0;
+var hours = 0;
+
+var disSec = "0";
+var disMin = "0";
+var disHr = "0";
+
+var interval = null;
 
 $("#btn-start").on("click", function(){
 	$(this).toggleClass("btn-blue");
 	$(this).toggleClass("btn-pink");
+	
+	//console.log($("#btn-start").attr("class"));
+	
+	if($("#btn-start").hasClass("btn-pink")) {
+		interval = window.setInterval(stopWatch, 100);
+	} else if($("#btn-start").hasClass("btn-blue")) {
+		window.clearInterval(interval);
+	}
 });
+
+function stopWatch() {
+	deciseconds++;
+	
+	if(deciseconds / 10 == 1){
+		deciseconds = 0;
+		seconds++;
+		
+		if(seconds / 60 == 1) {
+			seconds = 0;
+			minutes ++;
+			
+			if(minutes / 60 == 1) {
+				minutes = 0;
+				hours ++;
+			}
+		}
+	}
+	
+	if(seconds < 10) {
+		disSec = "0";
+	} else {
+		disSec = "";
+	}
+	if(minutes < 10) {
+		disMin = "0";
+	} else {
+		disMin = "";
+	}
+	if(hours < 10) {
+		disHr = "0";
+	} else { 
+		disHr = "";
+	}
+	
+	$("#display").html(disHr + hours + ":" + disMin + minutes + ":" + disSec + seconds + "." + deciseconds);
+}
