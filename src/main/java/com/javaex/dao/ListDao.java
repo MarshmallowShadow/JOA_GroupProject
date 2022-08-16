18f88a8bb4bae064389345ce8f694fae4bc8c21d
subintoto <subintoto@naver.com>
2022-08-16 14:11:28 +0900

    리스트

--- src/main/java/com/javaex/dao/ListDao.java
+++ src/main/java/com/javaex/dao/ListDao.java
@@ -1,5 +1,31 @@
 package com.javaex.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.ListVo;
+
+@Repository
 public class ListDao {
 
+	//필드
+	@Autowired
+	SqlSession sqlSession;
+	
+	//생성자
+	
+	//메소드
+	
+	//메소드 일반
+	
+	//글쓰기 등록
+	public int write(ListVo listVo) {
+		System.out.println("ListDao>write");
+		
+		int count = sqlSession.insert("list.listWrite", listVo);
+		
+		return count;
+	}
+	
 }
