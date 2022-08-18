00c8f2b5982d293a16f2b645cded3c06e94529e8
가은 <rkdms8228@naver.com>
2022-08-18 10:26:26 +0900

    함께하기 하는 중

--- src/main/java/com/javaex/controller/TogetherController.java
+++ src/main/java/com/javaex/controller/TogetherController.java
@@ -1,9 +1,14 @@
 package com.javaex.controller;
 
+
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.javaex.service.TogetherService;
 
@@ -12,8 +17,9 @@ import com.javaex.service.TogetherService;
 public class TogetherController {
 	
 	//필드
-	//@Autowired
-	//TogetherService togetherService;
+	@Autowired
+	TogetherService togetherService;
+	
 	//생성자
 	
 	
@@ -24,15 +30,23 @@ public class TogetherController {
 	
 	//함께하기 리스트
 	@RequestMapping(value="/together", method = {RequestMethod.GET, RequestMethod.POST})
-	public String together() {
+	public String together(Model model, @RequestParam(value="crtPage", required = false, defaultValue = "1")int crtPage) {
 		
 		 System.out.println("TogetherController > together");
+		 
+		 Map<String, Object> pMap = togetherService.together(crtPage);
+			
+			model.addAttribute("boardList", pMap.get("boardList"));
+			model.addAttribute("prev", pMap.get("prev"));
+			model.addAttribute("next", pMap.get("next"));
+			model.addAttribute("startPageBtnNo", pMap.get("startPageBtnNo"));
+			model.addAttribute("endPageBtnNo", pMap.get("endPageBtnNo"));
 		
 		return "together/together";
 	}
 	
 	//함께하기 글쓰기폼
-	@RequestMapping(value="/together-writeform", method = {RequestMethod.GET, RequestMethod.POST})
+	@RequestMapping(value="/writeform", method = {RequestMethod.GET, RequestMethod.POST})
 	public String writeform() {
 		
 		 System.out.println("TogetherController > writeform");
@@ -41,12 +55,12 @@ public class TogetherController {
 	}
 	
 	//함께하기 상세페이지
-	@RequestMapping(value="/together-read", method = {RequestMethod.GET, RequestMethod.POST})
+	@RequestMapping(value="/read", method = {RequestMethod.GET, RequestMethod.POST})
 	public String read() {
 		
 		 System.out.println("TogetherController > read");
 		
 		return "together/read";
 	}
-
+	
 }
