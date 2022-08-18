d0d2939f46625d00a906c22f9d8bdd9db2156b91
가은 <rkdms8228@naver.com>
2022-08-18 10:26:32 +0900

    함께하기 하는 중

--- src/main/java/com/javaex/service/TogetherService.java
+++ src/main/java/com/javaex/service/TogetherService.java
@@ -1,16 +1,22 @@
 package com.javaex.service;
 
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.javaex.dao.TogetherDao;
+import com.javaex.vo.EventVo;
 
 @Service
 public class TogetherService {
 	
 	//필드
-	//@Autowired
-	//TogetherDao togetherDao;
+	@Autowired
+	TogetherDao togetherDao;
 	
 	//생성자
 	
@@ -19,5 +25,85 @@ public class TogetherService {
 	
 	
 	//메소드-일반
+	public Map<String, Object> together(int crtPage) {
+		
+		System.out.println("TogetherService > together");
+		
+		//////////////////////////////////////////////
+		//                 리스트 가져오기               //
+		//////////////////////////////////////////////
+		
+		//페이지당 글 갯수
+		int listCnt = 10;
+		
+		//현재 페이지
+		//             조건문       true일 때 : false일 때
+		crtPage = (crtPage > 0) ? crtPage : (crtPage = 1);
+		
+		/*
+		if(crtPage>0) {
+			
+		}else {
+			crtPage = 1;
+		}
+		*/
+		
+		//시작글 번호
+		int startRnum = (crtPage-1)*listCnt + 1;
+		
+		//끝글 번호
+		int endRnum = (startRnum + listCnt) - 1;
+		
+		//System.out.println("글갯수" + listCnt + ",페이지" + crtPage + ",시작글" + startRnum + ",끝글" + endRnum);
+		
+		List<EventVo> togetherList = togetherDao.together(startRnum, endRnum);
+		
+		//System.out.println(boardList);
+		
+		//////////////////////////////////////////////
+		//                  페이징 계산                 //
+		//////////////////////////////////////////////
+		
+		//전체 글갯수
+		int totalCnt = togetherDao.selectTotalCnt();
+		System.out.println("service = " + totalCnt);
+		
+		//페이지당 버튼 갯수
+		int pageBtnCount = 10;
+		
+		//마지막 버튼 번호
+		int endPageBtnNo = (int)Math.ceil(crtPage/(double)pageBtnCount)*pageBtnCount;
+		
+		//시작 버튼 번호
+		int startPageBtnNo = (endPageBtnNo-pageBtnCount)+1;
+		
+		//다음 페이지 화살표 유무
+		boolean next = false;
+		
+		if((listCnt*endPageBtnNo) < totalCnt) {
+			next = true;
+		}else {
+			endPageBtnNo = (int)Math.ceil(totalCnt/(double)listCnt);
+		}
+		
+		//이전 페이지 화살표 유무
+		boolean prev = false;
+		
+		if(startPageBtnNo != 1) {
+			prev = true;
+		}
+		
+		System.out.println("페이지" + crtPage + ", 시작 버튼 번호" + startPageBtnNo + ", 마지막 버튼 번호" + endPageBtnNo + ", 이전" + prev + ", 다음" + next);
+		
+		Map<String, Object> pMap = new HashMap<String, Object>();
+		pMap.put("togetherList", togetherList);
+		pMap.put("prev", prev);
+		pMap.put("next", next);
+		pMap.put("endPageBtnNo", endPageBtnNo);
+		pMap.put("startPageBtnNo", startPageBtnNo);
+		
+		return pMap;
+		
+	}
 
 }
