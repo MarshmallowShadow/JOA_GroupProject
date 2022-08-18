23c043a462d26d289b934373d02b4be63651353e
MarshmallowShadow <choiboseung@naver.com>
2022-08-18 11:42:45 +0900

    와우

--- src/main/java/com/javaex/api/controller/ApiMapController.java
+++ src/main/java/com/javaex/api/controller/ApiMapController.java
@@ -5,8 +5,10 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.javaex.service.MapService;
 
@@ -17,8 +19,10 @@ public class ApiMapController {
 	@Autowired
 	private MapService mService;
 	
-	@RequestMapping(value="/getList")
-	public List<Map<String, Object>> getList(@ModelAttribute Map<String, Object> kMap) {
+	//지도 검색 결과 가져오기
+	@ResponseBody
+	@RequestMapping(value="/getList", method= {RequestMethod.GET, RequestMethod.POST})
+	public List<Map<String, Object>> getList(@RequestBody Map<String, Object> kMap) {
 		System.out.println("ApiMapController>getList");
 		
 		List<Map<String, Object>> cList = mService.getList(kMap);
