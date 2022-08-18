0bd63fefe08a2fcc1101b13efdf70a10dcaf9ef7
가은 <rkdms8228@naver.com>
2022-08-18 18:17:31 +0900

    함께하기 리스트

--- webapp/assets/js/together/together.js
+++ webapp/assets/js/together/together.js
@@ -9,7 +9,7 @@
 		console.log("jquery로 요청 data만 받는 요청");
 		
 		//리스트 요청 + 그리기
-		//fetchList();
+		fetchList();
 		
 	});
  
@@ -18,57 +18,77 @@
 		
 		$.ajax({
 			
-			url : "${pageContext.request.contextPath}/together/together",		
+			url : pageContext + "/api/together/getList", //컨트롤러 RequestMapping url 작성하기
 			type : "post",
-			//contentType : "application/json",
-			//data : {name: "홍길동"},
-			
+			contentType : "application/json", //@RequestBody로 파라미터 가져오기 위해 필수 (정보 보낼 거 없으면 필요없음)
+			data : JSON.stringify(crtPage), //@RequestBody로 데이터 보낼때 필수 (정보 보낼 거 없으면 필요없음)
+				//data: Vo //@ModelAttribute나 @RequestParam으로 데이터 보낼때 이용 (정보 보낼 거 없으면 필요없음)
 			dataType : "json",
-			success : function(togetherList){
-				//화면 data + html 그린다
-				for(var i=0; i<togetherList.length; i++){
-					render(togetherList[i], "down");  //vo --> 화면에 그린다.
+			success : function(result){
+				
+				console.log(result);
+				
+				//컨트롤러 함수 실행 후 코드
+				for(var i=0; i<result.togetherList.length; i++){
+					render(result.togetherList[i], "down");  //vo --> 화면에 그린다.
+				}
+				
+				if(result.prev == true){
+					renderPage(result.startPageBtnNo-1 , "◀");
+				}
+				
+				for(var i=result.startPageBtnNo; i<=result.endPageBtnNo; i++){
+					renderPage(i, i);
+				}
+				
+				if(result.next == true){
+					renderPage(result.endPageBtnNo+1 , "▶");
 				}
 			},
 			error : function(XHR, status, error) {
 				console.error(status + " : " + error);
 			}
+			
 		});
 		
 	}
  
  	/* 리스트 그리기 1개씩*/
-	function render(map, opt) {
+	function render(Map, opt) {
+		
+		var start = '송파'; //카카오 api로 위치 칮기 (togetherList.X1, togetherList.Y1)
+		var finish = '강남'; //카카오 api로 위치 칮기 (togetherList.X2, togetherList.Y2)
 		
 		console.log("render");
+		console.log(Map);
 		
 		var str = '' ;
 		str += ' <div class="chart"> ' ;
 		str += ' 	<table> ' ;
 		str += ' 		<thead> ' ;
 		str += ' 			<tr class="top"> ' ;
-		str += ' 				<th class="line_top">${tMap.REGSTART} - ${tMap.REGEND}</th> ' ;
+		str += ' 				<th class="line_top">'+Map.REGSTART+' - '+Map.REGEND+'</th> ' ;
 		str += ' 				<th class="mark"> ' ;
 		str += ' 					<button class="bookmark"> ' ;
-		str += ' 						<img class="image" src="${pageContext.request.contextPath}/assets/image/together/bookmarks_black.png"> ' ;
+		str += ' 						<img class="image" src="'+ pageContext +'/assets/image/together/bookmarks_black.png"> ' ;
 		str += ' 					</button> ' ;
 		str += ' 				</th> ' ;
 		str += ' 			</tr> ' ;
 		str += ' 		</thead> ' ;
 		str += ' 		<tbody> ' ;
 		str += ' 			<tr> ' ;
-		str += ' 				<th colspan="2" class="content">${tMap.TITLE}</th> ' ;
+		str += ' 				<th colspan="2" class="content">'+Map.TITLE+'</th> ' ;
 		str += ' 			</tr> ' ;
 		str += ' 			<tr> ' ;
 		str += ' 				<th colspan="2" class="content_course"> ' ;
-		str += ' 					<span class="glyphicon glyphicon-map-marker" class="marking"></span>${tMap.MINORDERNO} - ${tMap.MAXORDERNO} ' ;
+		str += ' 					<span class="glyphicon glyphicon-map-marker" class="marking"></span>'+ start +' - '+ finish +' ' ;
 		str += ' 					<input type="hidden" name="courseNo"> ' ;
 		str += ' 				</th> ' ;
 		str += ' 			</tr> ' ;
 		str += ' 			<tr> ' ;
 		str += ' 				<th colspan="2" class="line_bottom"> ' ;
-		str += ' 					${tMap.COURSECATEGORY} ' ;
-		str += ' 					<button type="submit" class="join"><span class="glyphicon glyphicon-user" id="join_icon"></span>${tMap.COUNT}/${tMap.JOINMAX}</button> ' ;
+		str += ' 					'+Map.COURSECATEGORY+' ' ;
+		str += ' 					<button type="submit" class="join"><span class="glyphicon glyphicon-user" id="join_icon"></span>'+Map.COUNT+'/'+Map.JOINMAX+'</button> ' ;
 		str += ' 				</th> ' ;
 		str += ' 			</tr> ' ;
 		str += ' 		</tbody> ' ;
@@ -85,6 +105,13 @@
 
  	}
  
+ 	function renderPage(pageNo, symbol){
+	
+		var str = '<li class="li"><a href="' + pageContext + '/together/together?crtPage=' + pageNo + '">' + symbol + '</a></li>';
+		
+		$(".ul").append(str);
+		
+	}
  
  
  
