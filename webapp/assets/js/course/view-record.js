6cc6603172e6ca1b276f207fc4353a7f7f348138
gmlwl106 <66778894@naver.com>
2022-08-22 17:53:04 +0900

    ㅇㅇ

--- webapp/assets/js/course/view-record.js
+++ webapp/assets/js/course/view-record.js
@@ -14,8 +14,8 @@ $(document).ready(function() {
 		/*https://lokeshdhakar.com/projects/lightbox2/*/
 		'alwaysShowNavOnTouchDevices': true, /*이미지 세트 일때 화면 양쪽 누르면 사진 변경*/
 		'disableScrolling': true, /*화면 스크롤링 막기*/
-		'fadeDuration': 300, /*페이드인아웃*/
-		'imageFadeDuration': 300, /*페이드인*/
+		'fadeDuration': 100, /*페이드인아웃*/
+		'imageFadeDuration': 100, /*페이드인*/
 		'fitImagesInViewport': true, /*이미지 크기 자동조절*/
 		'wrapAround': true, /*이미지 끝나도 다시 첫이미지로*/
 		'albumLabel': "총 %2개 중 %1번째 이미지"
