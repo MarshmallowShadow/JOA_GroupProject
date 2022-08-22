d068611ce5733a8779bf613458e9a030cc55895e
gmlwl106 <66778894@naver.com>
2022-08-22 17:25:38 +0900

    기록수정폼 이미지 하는중

--- webapp/assets/js/course/modify-record.js
+++ webapp/assets/js/course/modify-record.js
@@ -11,16 +11,32 @@
 	/*지도 그리기*/
 	map(courseNo);
 	
+	
+/********************************************************사진 드래그앤 드롭********************************************************/ 
+	
 	/*사진 가져오기*/
 	getImgs(recordNo);
+	
+	$("#input-file").bind('change', function() {
+		selectFile(this.files);
+	});
 	
 	
 	//업로드 파일 삭제
 	$("#filesList").on("click", ".close", function(e) {
 		var $target = $(e.target).parent();
-		var idx = $target.attr('data-idx');
-		console.log(idx);
-		delete uploadFiles[idx]; //배열에서 요소 삭제
+		var no = $target.attr('data-no');
+		
+		if(no == undefined) {
+			//새로 추가한 이미지 삭제
+			var idx = $target.attr('data-idx');
+			delete uploadFiles[idx]; //배열에서 요소 삭제
+			console.log("upload:"+uploadFiles);
+		} else {
+			//기존에 있던 이미지 삭제 (삭제할 배열에 추가)
+			deleteFiles.push(parseInt(no));
+			console.log("delete:"+deleteFiles);
+		}
 		
 		$target.parent().remove(); //프리뷰 삭제
 	});
@@ -119,7 +135,7 @@ function getImgs(recordNo) {
 				
 				for(var i=0; i<imgList.length; i++) {
 					
-					prevRender(imgList[i], i);
+					prevRender(imgList[i], imgList[i].recordImgNo);
 				}
 			}
 			
@@ -129,15 +145,120 @@ function getImgs(recordNo) {
 		}
 	});
 }
-
-function prevRender(imgVo, idx) {
+/*불러온 사진 미리보기*/
+function prevRender(imgVo, no) {
 	var div = '<div class="thumb">'+
-					'<div class="close" data-idx="'+idx+'">'+
+					'<div class="close" data-no="'+no+'">'+
 						'<span class="glyphicon glyphicon-remove">' +
 					'</div>'+
 					'<img src="'+contextPath+'/upload/'+imgVo.saveName+'"/>'+
 				'</div>';
 	$("#filesList").append(div);
+}
+
+
+$(function() {
+	//파일 드롭 다운
+	fileDropDown();
+});
+
+//파일 드롭 다운
+function fileDropDown() {
+	var dropZone = $("#drop");
+	
+	//drag 기능
+	dropZone.on('dragenter', function(e){
+		e.stopPropagation();
+		e.preventDefault();
+		//드롭다운 영역 css
+		//dropZone.css('background-color', '#FFFFFF');
+	});
+	//drag 뺐을때
+	dropZone.on('dragleave', function(e) {
+		e.stopPropagation();
+		e.preventDefault();
+		//드롭다운 영역 css
+		//dropZone.css('background-color', '#FFFFFF');
+	});
+	//drag 끝났을때
+	dropZone.on('dragover', function(e) {
+		e.stopPropagation();
+		e.preventDefault();
+		//드롭다운 영역 css
+		//dropZone.css('background-color', '#FFFFFF');
+	});
+	//drag한 파일 떨어뜨렸을때
+	dropZone.on('drop', function(e) {
+		e.preventDefault();
+		//드롭다운 영역 css
+		//dropZone.css('background-color', '#FFFFFF');
+		
+		var files = e.originalEvent.dataTransfer.files;
+		if(files != null) {
+			if(files.length < 1) {
+				console.log("폴더 업로드 불가");
+				return;
+			} else {
+				selectFile(files);
+			}
+		} else {
+			alert("ERROR");
+		}
+	});
+}
+
+//파일 선택시
+function selectFile(fileObject) {
+	console.log("ㅇㅅㅇ?");
+	var files = null;
+	
+	if(fileObject != null) {
+		//파일 Drag 이용하여 등록시
+		files = fileObject;
+	} else {
+		//직접 파일 등록시
+		files = $('#multipaartFileList_' + fileIndex)[0].files;
+		console.log(files);
+	}
+	
+	//다중파일 등록
+	if(files != null) {
+		
+		if(files != null && files.length > 0) {
+			$("#fileDragDesc").hide();
+			$("#filesList").show();
+		} else {
+			$("#fileDragDesc").show();
+			$("#filesList").hide();
+		}
+		
+		
+		for(var i = 0; i < files.length; i++) {
+			console.log(files[i]);
+			var file = files[i];
+			var size = uploadFiles.push(file); //업로드 목록에 추가
+			preview(file, size - 1); //미리보기 만들기
+		}
+		
+	}
+}	
+
+//미리보기 생성
+function preview(file, idx) {
+	console.log(uploadFiles);
+	var reader = new FileReader();
+	reader.onload = (function(f, idx) {
+		return function(e) {
+			var div = '<div class="thumb">'+
+							'<div class="close" data-idx="'+idx+'">'+
+								'<span class="glyphicon glyphicon-remove">' +
+							'</div>'+
+							'<img src="'+e.target.result+'"/>'+
+						'</div>';
+			$("#filesList").append(div);
+		};						
+	})(file,idx);
+	reader.readAsDataURL(file);
 }
 
 
