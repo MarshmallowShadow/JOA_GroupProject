c9eecb752b42853ee1275f8fc1befcc6952aaf80
가은 <rkdms8228@naver.com>
2022-08-23 16:07:20 +0900

    멘션 빼고 완성

--- src/main/java/com/javaex/dao/EventCommentDao.java
+++ src/main/java/com/javaex/dao/EventCommentDao.java
@@ -1,8 +1,42 @@
 package com.javaex.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.EventCommentVo;
 
 @Repository
 public class EventCommentDao {
+	
+	//필드
+	@Autowired
+	SqlSession sqlSession;
+	
+	//생성자
+	
+	//메소드-gs
+	
+	
+	//메소드-일반
+	//댓글 쓰기
+	public int commentWrite(EventCommentVo eventCommentVo) {
+		
+		System.out.println("EventCommentDao > commentWrite");
+		
+		return sqlSession.insert("eventComment.commentWrite", eventCommentVo);
+		
+	}
+	
+	//댓글 읽기
+	public List<EventCommentVo> comment(int no) {
+		
+		System.out.println("EventCommentDao > comment");
+		
+		return sqlSession.selectList("eventComment.comment", no);
+		
+	}
 
 }
