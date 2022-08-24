fd9e231855d266f3da8a9635d69b8567a22efd7a
GyeongAe Kim <kgae128@gmail.com>
2022-08-24 16:46:09 +0900

    0824히히

--- src/main/java/com/javaex/controller/MypageController.java
+++ src/main/java/com/javaex/controller/MypageController.java
@@ -9,11 +9,13 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.javaex.service.MypageService;
+import com.javaex.service.UserService;
 import com.javaex.vo.UserVo;
 
 @Controller
@@ -21,6 +23,7 @@ import com.javaex.vo.UserVo;
 public class MypageController {
 	
 	@Autowired private MypageService mypageService;
+	
 	
 	//마이페이지-메인
 	@RequestMapping(value="", method = {RequestMethod.GET, RequestMethod.POST})
