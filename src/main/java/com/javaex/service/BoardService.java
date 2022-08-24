47abc8de5abb2be2da15e546d66f2ebd84d79b9f
가은 <rkdms8228@naver.com>
2022-08-24 18:31:01 +0900

    하는 중

--- src/main/java/com/javaex/service/BoardService.java
+++ src/main/java/com/javaex/service/BoardService.java
@@ -1,28 +1,37 @@
 package com.javaex.service;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.javaex.dao.BoardCommentDao;
 import com.javaex.dao.BoardDao;
+import com.javaex.dao.BoardImgDao;
 import com.javaex.vo.BoardCommentVo;
+import com.javaex.vo.BoardImgVo;
 import com.javaex.vo.BoardVo;
-import com.javaex.vo.EventCommentVo;
 
 @Service
 public class BoardService {
 	
 	//필드
 	@Autowired
-	BoardDao boardDao;
+	private BoardDao boardDao;
 	@Autowired
 	private BoardCommentDao boardCommentDao;
 	//@Autowired
 	//CourseDao courseDao;
+	@Autowired
+	private BoardImgDao boardImgDao;
 	
 	//생성자
 	
@@ -147,6 +156,9 @@ public class BoardService {
 		}
 		*/
 		
+		//줄바꿈
+		boardVo.setContent(boardVo.getContent().replace("\n", "<br>"));
+		
 		int count = boardDao.write(boardVo);
 		
 		//courseDao.selectCourse(boardVo);
@@ -155,7 +167,71 @@ public class BoardService {
 		
 		//System.out.println(count);
 
-		return count;
+		int no = boardVo.getBoardNo();
+		
+		System.out.println(no);
+		
+		return no;
+		
+	}
+	
+	//코스기록 이미지 등록
+	public String boardImgWrite(List<MultipartFile> fileList, int boardNo) {
+		
+		System.out.println("boardImgWrite > boardImgWrite");
+		
+		String saveDir = "C:\\javaStudy\\upload";
+		
+		//기록 이미지 번호가 있는지 찾음
+		int index = 0;
+		
+		for(MultipartFile file : fileList) {
+			
+			if(file.getSize() > 0) {
+				
+				//오리지널 파일명
+				String orgName = file.getOriginalFilename();
+				
+				//확장자
+				String exName = orgName.substring(orgName.lastIndexOf("."));
+				
+				//현재시간 + 랜덤UUID + 확장자
+				String saveName = System.currentTimeMillis() + UUID.randomUUID().toString() + exName;
+				
+				//파일경로(디렉토리 + 저장파일명)
+				String filePath = saveDir + "\\" + saveName;
+				
+				//DB 저장
+				BoardImgVo boardImgVo = new BoardImgVo();
+				boardImgVo.setBoardNo(boardNo);
+				boardImgVo.setSaveName(saveName);
+				boardImgVo.setFilePath("/upload/" + saveName);
+				boardImgVo.setOrderNo(index);
+				
+				System.out.println(boardImgVo);
+				boardImgDao.boardImg(boardImgVo);
+				
+				//파일 저장
+				try {
+					
+					byte[] fileData = file.getBytes();
+					OutputStream os = new FileOutputStream(filePath);
+					BufferedOutputStream bos = new BufferedOutputStream(os);
+					
+					bos.write(fileData);
+					bos.close();
+					
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+					return "false";
+				}
+			}
+			
+			index++;
+		}
+		
+		return "success";
 		
 	}
 	
@@ -180,10 +256,13 @@ public class BoardService {
 		//댓글
 		List<BoardCommentVo> boardCommentList = boardCommentDao.comment(no);
 		
+		List<BoardImgVo> imgList = boardImgDao.getImage(no);
+		
 		Map<String, Object> rMap = new HashMap<String, Object>();
 		rMap.put("bMap", bMap);
 		rMap.put("categoryMap", categoryMap);
 		rMap.put("boardCommentList", boardCommentList);
+		rMap.put("imgList", imgList);
 		
 		System.out.println(rMap);
 		
