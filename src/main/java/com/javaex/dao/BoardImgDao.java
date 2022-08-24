d1c62ddbb21fa83918b94cb61b025c64a93bc9c5
가은 <rkdms8228@naver.com>
2022-08-24 18:31:30 +0900

    하는 중

--- src/main/java/com/javaex/dao/BoardImgDao.java
+++ src/main/java/com/javaex/dao/BoardImgDao.java
@@ -1,8 +1,46 @@
 package com.javaex.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.BoardImgVo;
 
 @Repository
 public class BoardImgDao {
+	
+	//필드
+	@Autowired
+	SqlSession sqlSession;
+		
+	//생성자
+	
+	//메소드-gs
+	
+	
+	//메소드-일반
+	//이미지 번호 가져오기
+
+	//기록 이미지 추가
+	public int boardImg(BoardImgVo boardImgVo) {
+		
+		System.out.println("BoardImgDao > boardImg");
+		
+		return sqlSession.insert("boardImg.insert", boardImgVo);
+		
+	}
+	
+	//이미지 가져오기
+	public List<BoardImgVo> getImage(int boardNo) {
+		
+		System.out.println("BoardImgDao > getImage");
+		
+		List<BoardImgVo> imgList = sqlSession.selectList("boardImg.getImage", boardNo);
+		
+		return imgList;
+		
+	}
 
 }
