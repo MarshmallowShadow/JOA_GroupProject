f4178b2e82a5ee3f35f492b5d7cb83e101fc9904
가은 <rkdms8228@naver.com>
2022-08-25 18:46:50 +0900

    함께하기 지도 이제 나옴

--- src/main/java/com/javaex/api/controller/ApiTogetherController.java
+++ src/main/java/com/javaex/api/controller/ApiTogetherController.java
@@ -1,14 +1,19 @@
 package com.javaex.api.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.javaex.service.TogetherService;
+import com.javaex.vo.PointVo;
 
 @Controller
 @RequestMapping(value="/api/together")
@@ -28,5 +33,17 @@ public class ApiTogetherController {
 		return xMap;
 		
 	}
+		
+	//함께하기 상세페이지 지도 가져오기
+	@ResponseBody
+	@RequestMapping(value="/map", method = {RequestMethod.GET, RequestMethod.POST})
+	public List<PointVo> map(@RequestParam(value = "eventNo", required = false) int eventNo) {
+		
+		 System.out.println("ApiTogetherController > map");
+		 System.out.println(eventNo);
+		return togetherService.map(eventNo);
+		
+	}
+	
 	
 }
