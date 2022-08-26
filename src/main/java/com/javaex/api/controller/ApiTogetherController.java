8136a76321a4d680502b67a242abe71b166feda5
가은 <rkdms8228@naver.com>
2022-08-26 15:18:49 +0900

    태그 하는 중

--- src/main/java/com/javaex/api/controller/ApiTogetherController.java
+++ src/main/java/com/javaex/api/controller/ApiTogetherController.java
@@ -3,9 +3,10 @@ package com.javaex.api.controller;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.javaex.service.TogetherService;
 import com.javaex.vo.PointVo;
+import com.javaex.vo.UserVo;
 
 @Controller
 @RequestMapping(value="/api/together")
@@ -24,9 +26,19 @@ public class ApiTogetherController {
 	
 	@ResponseBody
 	@RequestMapping(value="/getList")
-	public Map<String, Object> getList (@RequestBody int crtPage){
+	public Map<String, Object> getList (@RequestBody int crtPage, HttpSession session){
 		
-		Map<String, Object> xMap = togetherService.together(crtPage);
+		System.out.println("ApiTogetherController > getList");
+		
+		UserVo authUser = (UserVo)session.getAttribute("authUser");
+		
+		int userNo = 0;
+		
+		if(authUser != null) {
+			userNo = authUser.getUserNo();
+		}
+		
+		Map<String, Object> xMap = togetherService.together(crtPage, userNo);
 		
 		System.out.println(xMap);
 		
@@ -40,7 +52,9 @@ public class ApiTogetherController {
 	public List<PointVo> map(@RequestParam(value = "eventNo", required = false) int eventNo) {
 		
 		 System.out.println("ApiTogetherController > map");
+		 
 		 System.out.println(eventNo);
+		 
 		return togetherService.map(eventNo);
 		
 	}
