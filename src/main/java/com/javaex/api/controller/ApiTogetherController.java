4a210928b9b73608e33570edb9218f68205bb9e7
가은 <rkdms8228@naver.com>
2022-08-26 20:09:09 +0900

    이제 태그됨

--- src/main/java/com/javaex/api/controller/ApiTogetherController.java
+++ src/main/java/com/javaex/api/controller/ApiTogetherController.java
@@ -21,12 +21,19 @@ import com.javaex.vo.UserVo;
 @RequestMapping(value="/api/together")
 public class ApiTogetherController {
 	
+	//필드
 	@Autowired
 	TogetherService togetherService;
 	
+	//생성자
+	
+	//메소드-gs
+	
+	//메소드-일반
+	//함께하기 리스트
 	@ResponseBody
-	@RequestMapping(value="/getList")
-	public Map<String, Object> getList (@RequestBody int crtPage, HttpSession session){
+	@RequestMapping(value="/getList", method = {RequestMethod.GET, RequestMethod.POST})
+	public Map<String, Object> getList(@RequestBody int crtPage, HttpSession session){
 		
 		System.out.println("ApiTogetherController > getList");
 		
@@ -43,6 +50,21 @@ public class ApiTogetherController {
 		System.out.println(xMap);
 		
 		return xMap;
+		
+	}
+	
+	//함께하기 태그 저장
+	@ResponseBody
+	@RequestMapping(value="/bookmark", method = {RequestMethod.GET, RequestMethod.POST})
+	public int bookmark(@RequestBody Map<String,Object> map){
+		
+		System.out.println("ApiTogetherController > bookmark");
+		
+		int changed = togetherService.bookmark(map);
+		
+		System.out.println(changed);
+		
+		return changed;
 		
 	}
 		
