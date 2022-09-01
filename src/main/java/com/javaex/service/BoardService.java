9087f8a2d98ae33326c83f01af6f68962e844933
가은 <rkdms8228@naver.com>
2022-09-02 08:18:20 +0900

    .

--- src/main/java/com/javaex/service/BoardService.java
+++ src/main/java/com/javaex/service/BoardService.java
@@ -161,23 +161,10 @@ public class BoardService {
 		
 		System.out.println("BoardService > write");
 		
-		/*
-		for(int i=1; i<30; i++) { //글쓰기 조작
-			boardVo.setBoardCategory("소통");
-			boardVo.setTitle(i+" 번째 게시물 제목입니다.");
-			boardVo.setContent(i+" 번째 게시물 내용입니다.");
-			boardDao.write(boardVo);
-		}
-		*/
-		
 		//줄바꿈
 		boardVo.setContent(boardVo.getContent().replace("\n", "<br>"));
 		
 		int count = boardDao.write(boardVo);
-		
-		//togetherDao.together(boardVo);
-		
-		//System.out.println(count);
 
 		int no = boardVo.getBoardNo();
 		
