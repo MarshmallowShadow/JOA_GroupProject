0a66680c5993ad16f05e744025aaff4d37a4ffc4
gmlwl106 <66778894@naver.com>
2022-10-24 16:21:47 +0900

    ㅇㅇ

--- src/main/java/com/javaex/controller/StatisticsController.java
+++ src/main/java/com/javaex/controller/StatisticsController.java
@@ -12,14 +12,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.javaex.service.StatisticsService;
 
 @Controller
-@RequestMapping(value="stat")
 public class StatisticsController {
 	
 	@Autowired
 	StatisticsService statService;
 
 	//통계 페이지
-	@RequestMapping(value="/", method = {RequestMethod.GET, RequestMethod.POST})
+	@RequestMapping(value="stat", method = {RequestMethod.GET, RequestMethod.POST})
 	public String Statistics(Model model) {
 		List<Map<String, Object>> statList = statService.getStat();
 		model.addAttribute("statList", statList);
