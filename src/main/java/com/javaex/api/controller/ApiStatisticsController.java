9a9d4b21d2a254a297e09bb18d112007d21548dc
gmlwl106 <66778894@naver.com>
2022-10-25 10:16:14 +0900

    통계api

---
+++ src/main/java/com/javaex/api/controller/ApiStatisticsController.java
@@ -0,0 +1,27 @@
+package com.javaex.api.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.javaex.service.StatisticsService;
+
+@Controller
+@RequestMapping(value="api")
+public class ApiStatisticsController {
+	
+	@Autowired
+	StatisticsService statService;
+
+	//통계 페이지
+	@ResponseBody
+	@RequestMapping(value="stat", method = {RequestMethod.GET, RequestMethod.POST})
+	public List<Map<String, Object>> Statistics() {
+		return statService.getStat();
+	}
+}
